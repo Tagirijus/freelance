d9155e5bd06515d32b7c8a901ddd5530a8d2e4e3
Manuel <info@tagirijus.de>
2017-04-18 13:24:54 +0200

    View deleted and switch to Python 3 started

--- offer/main.py
+++ offer/main.py
@@ -1,25 +1,9 @@
-# coding=utf-8
-
 """The main programm is executed here."""
 
-import curses
-from offer import view
+import npyscreen
 # from offer.entries import SimpleEntry
-
-
-def main(screen):
-    """Main process for the GUI handling."""
-    # init the window
-    w = view.OfferCurses()
-
-    # main loop for the curses modul
-    while 1:
-        w.output_table_line([1, 2, 3], col_widths=[10, 70, 30])
-        s = w.input(y=10, x=10)
-        if s == '':
-            break
 
 
 def run():
     """Execute the programm via curses.wrapper()."""
-    curses.wrapper(main)
+    print('Testing ...')
