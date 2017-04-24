d55ae6d8d638a412a8c84278654fbc109bbd8b57
Manuel <info@tagirijus.de>
2017-04-24 22:57:24 +0200

    - Changed class name ModEntry -> ConnectEntry - New class: MultiplyEntry - Improvements

--- offer/entries.py
+++ offer/entries.py
@@ -1,7 +1,7 @@
 """The classes for entries."""
 
 from decimal import Decimal
-from offer import time
+from offer import time as time_module
 import uuid
 
 
@@ -10,10 +10,10 @@ class BaseEntry(object):
 
     def __init__(
         self,
-        title='Fixed entry',
+        title='Base entry',
         comment='',
-        amount=0.0,
-        time_input=0.0,
+        amount=1.0,
+        time=0.0,
         price=0.00
     ):
         """Init the class."""
@@ -21,10 +21,11 @@ class BaseEntry(object):
         self._comment = str(comment)
         self._amount = Decimal('0.0')               # set default
         self.set_amount(amount)                     # try to set arguments value
-        self._time = time.to_timedelta(time_input)
+        self._time = time_module.to_timedelta(time)
         self._price = Decimal('0.00')               # set default
         self.set_price(price)                       # try to set arguments value
         self._id = str(uuid.uuid1())
+        self._connected = set()
 
     def get_comment(self):
         """Get comment."""
@@ -66,19 +67,21 @@ class BaseEntry(object):
             # otherwise don't do anything
             pass
 
-    def get_time(self):
+    def get_time(self, *args, **kwargs):
         """Get time."""
         return self._time
 
     def set_time(self, value):
         """Set time."""
-        self._time = time.to_timedelta(value)
+        self._time = time_module.to_timedelta(value)
 
-    def get_hours(self):
+    def get_hours(self, *args, **kwargs):
         """Get hours as decimal."""
-        return time.get_decimal_hours_from_timedelta(self._time)
+        return time_module.get_decimal_hours_from_timedelta(
+            self.get_time()
+        )
 
-    def get_price(self):
+    def get_price(self, *args, **kwargs):
         """Get price."""
         return self._price
 
@@ -96,23 +99,117 @@ class BaseEntry(object):
         """Get id."""
         return self._id
 
+    def get_connected(self):
+        """Get connected list."""
+        return self._connected
 
-class ModEntry(BaseEntry):
-    """Entry for modulating respecting other entries."""
+
+class ConnectEntry(BaseEntry):
+    """
+    This entry type can connect to other entries.
+
+    It's meant to act as a modulator respecting other entries.
+    If it is connected to other entries, it calculates time
+    and price according to the time and price of the connected
+    entries multiplied by the own multiplicator.
+    """
 
     def __init__(
         self,
-        title='Mod entry',
+        title='Connect entry',
+        comment='',
+        amount=1.0,
         is_time=True,
         multiplicator=1.0
     ):
         """Initialize the class."""
-        super(ModEntry, self).__init__()
+        super(ConnectEntry, self).__init__()
         self._title = str(title)
-        self._connected = []
+        self._comment = str(comment)
+        self._amount = Decimal('0.0')               # set default
+        self.set_amount(amount)                     # try to set arguments value
         self._is_time = bool(is_time)
         self._multiplicator = Decimal('1.0')  # set default
         self.set_multiplicator(multiplicator)  # try to set arguments value
+
+    def get_time(self, entry_list=None):
+        """
+        Get time according to entry_list or zero.
+
+        entry_list is the global list holding all entry-objects.
+        This function calculates the time respecting the entries from the
+        entry_list. The ConnectEntry class is an entry for multiplying entries
+        times or only prices (depenging on self.is_time()) - e.g. licences
+        can cost x-times multiplied the original working time of a task.
+        This class can calculate it.
+        """
+        # if is_time() == False, return 0 timedelta
+        if not self.is_time() or type(entry_list) is None:
+            return time_module.to_timedelta(0)
+        # is_time() == True, calculate time respecting other entires
+        else:
+            # cancel and return zero, if entry_list is no list
+            if type(entry_list) != list:
+                return time_module.to_timedelta(0)
+
+            # otherwise iterate through entry_list and find
+            # entries which ids exist in the self._connected list
+            out = time_module.to_timedelta(0)
+            for entry in entry_list:
+                if entry.get_id() in self._connected:
+                    # if its in the list, multiply its time and add it
+                    out += (float(self.get_multiplicator()) *
+                            entry.get_time(entry_list=entry_list))
+            # return the result
+            return out * float(self.get_amount())
+
+    def get_hours(self, entry_list=None):
+        """Get hours as decimal."""
+        if type(entry_list) is None:
+            return Decimal('0.0')
+        else:
+            return time_module.get_decimal_hours_from_timedelta(
+                self.get_time(entry_list=entry_list)
+            )
+
+    def set_time(self, value):
+        """Disable the function."""
+        pass
+
+    def get_price(self, entry_list=None, wage=Decimal('0.00')):
+        """
+        Get price according to entry_list or self.get_time().
+
+        entry_list is the global list holding all entry-objects.
+        Depending on self.is_time() this function calculates the price
+        according to the entry_list. It is similar to self.get_time(),
+        but it just calculates the prices instead of the time_module.
+        wage should be a Decimal() object.
+        """
+        # if no list is given, return zero
+        if type(entry_list) is None:
+            return Decimal('0.00')
+        # if self.is_time() == True, just multiply self.get_hours() * wage
+        if self.is_time():
+            return round(self.get_hours(entry_list=entry_list) * wage, 2)
+        else:
+            # otherwise iterate through entry_list and find prices of
+            # entries which ids exist in the self._connected list
+            # and multiply them
+            out = Decimal(0)
+            for entry in entry_list:
+                if entry.get_id() in self._connected:
+                    # if its in the list, multiply its price and add it
+                    out += (self.get_multiplicator() *
+                            entry.get_price(
+                                entry_list=entry_list,
+                                wage=wage))
+            # return the value
+            return round(out * self.get_amount(), 2)
+
+    def set_price(self, value):
+        """Disable function."""
+        pass
 
     def get_multiplicator(self):
         """Get multiplicator."""
@@ -129,25 +226,116 @@ class ModEntry(BaseEntry):
             pass
 
     def is_time(self, set_it=None):
-        """Get or set is_time value."""
-        if set_it == None:
-            # set_it not set, so show me the value
-            return self._is_time
+        """Get is_time value."""
+        return self._is_time
+
+    def set_is_time(self, value):
+        """Set is_time value."""
+        self._is_time = bool(value)
+
+    def connect_entry(self, entry_list=None, entry_id=None, disconnect=False):
+        """
+        Append entry to the self._connected set.
+
+        This function searches for the entry with the ID == entry_id
+        in the entry_list. If it exists, it checks if the own ID does
+        not exists in the found entry.get_connected() set and appends
+        it to the own self._connected set then, if it.
+        """
+        # if one argument is not given, cancel
+        if type(entry_list) is None or type(entry_id) is None:
+            return
+
+        # also cancel if entry_id is the own
+        if entry_id == self.get_id():
+            return
+
+        # check if other entry id exists in entry_list
+        connection_possible = False
+        for index, entry in enumerate(entry_list):
+            if entry.get_id() == entry_id:
+                # and also check if the entry has self.get_id()
+                # in its get_connected() set.
+                if self.get_id() not in entry.get_connected():
+                    connection_possible = True
+
+        # cancel if nothing found
+        if not connection_possible:
+            return
+
+        # append / "connect" id to _connected set
+        # or remove / "discconnect" id to _connected set
+        if disconnect:
+            self._connected -= set([entry_id])
         else:
-            # parameter given, set the value
-            self._is_time = bool(set_it)
-            # and return it afterwards
-            return self._is_time
+            self._connected |= set([entry_id])
 
-    def get_connected(self):
-        """Get connected list."""
-        return self._connected
+    def disconnect_entry(self, entry_list=None, entry_id=None):
+        """Delete the entry_id from the _connected set."""
+        self.connect_entry(
+            entry_list=entry_list,
+            entry_id=entry_id,
+            disconnect=True
+        )
 
-    def connect_entry(self, entry_id):
-        """Append to the self._connected list."""
-        self._connected.append(str(entry_id))
 
-    def disconnect_entry(self, entry_id):
-        """Delete the entry_id from the self._connected list."""
-        if str(entry_id) in self._connected:
-            self._connected.pop(self._connected.index(str(entry_id)))
+class MultiplyEntry(BaseEntry):
+    """
+    A multiplying entry.
+
+    This entry type is similar to the BaseEntry, except that
+    it calculates the time according to a given hour_rate value.
+    The class has a new value "hour_rate" which will be multiplicated
+    by the amount and thus calculates the time.
+    """
+
+    def __init__(
+        self,
+        title='Multiply entry',
+        comment='',
+        amount=1.0,
+        hour_rate=1.0
+    ):
+        """Initialize the class."""
+        super(MultiplyEntry, self).__init__()
+        self._title = str(title)
+        self._comment = str(comment)
+        self._amount = Decimal('0.0')               # set default
+        self.set_amount(amount)                     # try to set arguments value
+        self._hour_rate = time_module.to_timedelta(hour_rate)
+
+    def get_time(self, *args, **kwargs):
+        """Get own amount * own hour as time."""
+        return float(self.get_amount()) * self.get_hour_rate()
+
+    def set_time(self, value):
+        """Disable the function."""
+        pass
+
+    def get_hours(self, *args, **kwargs):
+        """Get hours as decimal."""
+        return time_module.get_decimal_hours_from_timedelta(
+            self.get_time()
+        )
+
+    def get_price(self, wage=Decimal('0.00'), *args, **kwargs):
+        """Get own time * wage as price."""
+        return round(self.get_hours() * wage, 2)
+
+    def set_price(self, value):
+        """Disable function."""
+        pass
+
+    def get_hour_rate(self):
+        """Get hour_rate."""
+        return self._hour_rate
+
+    def set_hour_rate(self, value):
+        """Set hour_rate."""
+        # try to set a new hour_rate
+        try:
+            # only works, if input is integer, float or string
+            self._hour_rate = Decimal(str(value))
+        except Exception:
+            # otherwise don't do anything
+            pass
--- offer/main.py
+++ offer/main.py
@@ -1,7 +1,9 @@
 """The main programm is executed here."""
 
 from offer.entries import BaseEntry
-from offer.entries import ModEntry
+from offer.entries import ConnectEntry
+from offer.entries import MultiplyEntry
+from decimal import Decimal
 import os
 
 
@@ -12,5 +14,53 @@ BASE_PATH = os.path.dirname(os.path.realpath(__file__))[
 
 def main():
     """Run the programm."""
-    a = ModEntry()
-    print(a.is_time())
+    a = BaseEntry(
+        title='Base Eintrag A',
+        comment='Kommentar Eintrag A',
+        amount=1.0,
+        time=1.0,
+        price=50.00
+    )
+
+    b = ConnectEntry(
+        title='Connect Eintrag B',
+        comment='Kommentar Eintrag B',
+        amount=2.5,
+        is_time=False,
+        multiplicator=0.5
+    )
+
+    c = ConnectEntry(
+        title='Connect Eintrag C',
+        comment='Kommentar Eintrag C',
+        amount=1.0,
+        is_time=False,
+        multiplicator=2.0
+    )
+
+    d = MultiplyEntry(
+        title='Multiply Eintrag D',
+        comment='Kommentar Eintrag D',
+        amount=2.0,
+        hour_rate=0.5
+    )
+
+    entries = []
+    entries.append(a)
+    entries.append(b)
+    entries.append(c)
+    entries.append(d)
+    entries[1].connect_entry(entries, entries[0].get_id())
+    entries[2].connect_entry(entries, entries[0].get_id())
+    entries[2].connect_entry(entries, entries[1].get_id())
+    wage = Decimal('50.00')
+
+    for e in entries:
+        out = []
+        out.append(str(e.get_title()))
+        out.append(str(e.get_comment()))
+        out.append(str(e.get_amount()))
+        out.append(str(e.get_time(entry_list=entries)))
+        out.append(str(e.get_price(wage=wage, entry_list=entries)))
+
+        print(', '.join(out))
